71505a21fef5c0f5e1db94a6b1e82897d23830c4
Théo Guérin <57496441+TheoGuerin64@users.noreply.github.com>
2023-09-20 23:32:16 +0200

    Improve player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,16 +5,16 @@ const SPEED = 300.0
 
 
 func _physics_process(_delta):
-	var xDirection = Input.get_axis("game_left", "game_right")
-	if xDirection:
-		velocity.x = xDirection * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	velocity.x = 0
+	if Input.is_action_pressed("game_left"):
+		velocity.x = -SPEED
+	elif Input.is_action_pressed("game_right"):
+		velocity.x = SPEED
 
-	var yDirection = Input.get_axis("game_up", "game_down")
-	if yDirection:
-		velocity.y = yDirection * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+	velocity.y = 0
+	if Input.is_action_pressed("game_up"):
+		velocity.y = -SPEED
+	elif Input.is_action_pressed("game_down"):
+		velocity.y = SPEED
 
 	move_and_slide()
